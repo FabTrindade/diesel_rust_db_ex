42d65c34c5b28b75b4efc236402d09fcbc1cb884
Fabricio Trindade Costa <fabricio.trindade@ifood.com.br>
2024-12-16 14:31:49 -0300

    Configuring diesel up.sql and down.sql files and migration execution.

--- migrations/2024-12-16-164000_create_usuarios/up.sql
+++ migrations/2024-12-16-164000_create_usuarios/up.sql
@@ -1,1 +1,7 @@
--- Your SQL goes here
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    user_name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+);
+
+SELECT diesel_manage_updated_at('users');
